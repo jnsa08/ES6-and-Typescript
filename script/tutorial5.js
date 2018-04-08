ff2b92a2b464f620c25cb51418c979343aaa67d9
Jonathan <jonathan.saavedra08@gmail.com>
2018-04-08 20:42:47 -0300

    Turial 5

---
+++ script/tutorial5.js
@@ -0,0 +1,12 @@
+function greetPerson(name) {
+    if (name === "Chandler") {
+        greet = "Hello Chandler";
+    }
+    else {
+        greet = "Hi there";
+    }
+    console.log(greet);
+    var greet;
+}
+greetPerson("Chandler");
+//# sourceMappingURL=tutorial5.js.map
---
+++ tutorial5.ts
@@ -0,0 +1,11 @@
+function greetPerson(name: string) {
+    if (name === "Chandler") {
+        var greet = "Hello Chandler";
+    } else {
+        var greet = "Hi there";
+    }
+    console.log(greet);
+
+}
+
+greetPerson("Chandler");
