1bbeabf6d651a47e351ba283412c8526ac3969fc
Jonathan <jonathan.saavedra08@gmail.com>
2018-04-08 21:20:21 -0300

    Tutorial 6, diferencia entra var y let

---
+++ script/tutorial6.js
@@ -0,0 +1,22 @@
+function greetPerson(name) {
+    var greet;
+    if (name === "Chandler") {
+        greet = "Hello Chandler";
+    }
+    else {
+        greet = "Hi there";
+    }
+    console.log(greet);
+}
+greetPerson("Chandler");
+var a = 1;
+var b = 2;
+if (a === 1) {
+    var a = 10;
+    var b_1 = 20;
+    console.log(a);
+    console.log(b_1);
+}
+console.log(a);
+console.log(b);
+//# sourceMappingURL=tutorial6.js.map
