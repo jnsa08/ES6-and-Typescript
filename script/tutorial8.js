14ba903d1c7a16512d31fe786a62bfaad57a763d
Jonathan <jonathan.saavedra08@gmail.com>
2018-04-09 23:05:30 -0300

    Tutorial 8, uso keyword "Const"

---
+++ script/tutorial8.js
@@ -0,0 +1,9 @@
+var obj1 = {
+    name: "jonathan"
+};
+console.log(obj1.name);
+//Si deseo hacer una nueva asignacion al objeto obj1, tendria que cambiar const por let.
+//O asignar directamente a la propiedad del objeto
+obj1.name = "Saavedra";
+console.log(obj1.name);
+//# sourceMappingURL=tutorial8.js.map
---
+++ tutorial8.ts
@@ -0,0 +1,14 @@
+
+const obj1 =  {
+    name : "jonathan"
+}
+
+console.log(obj1.name);
+
+
+//Si deseo hacer una nueva asignacion al objeto obj1, tendria que cambiar const por let.
+//O asignar directamente a la propiedad del objeto
+
+
+obj1.name= "Saavedra";
+console.log(obj1.name);
